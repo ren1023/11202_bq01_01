61684103eaa9e38c29970544c727d89abcaf34c9
ren1023 <renyou.1023@gmail.com>
2024-03-11 16:24:25 +0800

    fix:將&#39;以'符替代

--- back/title.php
+++ back/title.php
@@ -13,7 +13,7 @@
 	</table>
 	<div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
 		<p class="t cent botli">網站標題管理</p>
-		<form method="post" target="back" action="?do=tii">
+		<form method="post" target="back" action="./api/edit.php">
 			<table width="100%">
 				<tbody>
 					<tr class="yel">
@@ -28,8 +28,13 @@
 			<table style="margin-top:40px; width:70%;">
 				<tbody>
 					<tr>
-						<td width="200px"><input type="button" onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;view.php?do=title&#39;)" value="新增網站標題圖片"></td>
-						<td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
+						<td width="200px">
+							<input type="button" onclick="op('#cover','#cvr','view.php?do=title')" value="新增網站標題圖片">
+						</td>
+						<td class="cent">
+							<input type="submit" value="修改確定">
+							<input type="reset" value="重置">
+						</td>
 					</tr>
 				</tbody>
 			</table>
